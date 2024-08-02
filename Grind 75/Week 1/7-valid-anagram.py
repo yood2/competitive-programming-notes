47342c69e5f9943067a26db55b758943ba3f7ab4
yood2 <yood2@proton.me>
2024-08-02 16:58:09 -0700

    Finished q7, week 1

--- Grind 75/Week 1/7-valid-anagram.py
+++ Grind 75/Week 1/7-valid-anagram.py
@@ -16,6 +16,39 @@ Constraints:
 s and t consist of lowercase English letters.
 '''
 
+# Intuition: Check length, convert s into hash with occurance values, remove by occurence in t, check if hash is empty
 def isAnagram(s: str, t: str):
+    if len(s) != len(t):
+        return False
+
+    hash = {}
+
+    for c in s:
+        if c in hash:
+            hash[c] += 1
+        else:
+            hash[c] = 1
     
-    return False
+    for c in t:
+        if c not in hash.keys():
+            return False
+        elif hash[c] < 1:
+            return False
+        hash[c] -= 1
+        if hash[c] == 0:
+            hash.pop(c)
+    
+    return len(hash) == 0
+
+# Solution: Counter Hash Algo
+def isAnagram(s: str, t: str):
+    sHash = defaultdict(int)
+    tHash = defaultdict(int)
+
+    for c in s:
+        sHash[c] += 1
+    
+    for c in t:
+        tHash[c] += 1
+
+    return sHash == tHash
