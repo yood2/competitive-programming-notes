2c9b5e898fd94afc1096215d3c09d14a02c2e794
yood2 <yood2@proton.me>
2024-09-15 12:19:29 -0700

    made review.cpp that covers chapter 1 of cses handbook

--- cses/review.cpp
+++ cses/review.cpp
@@ -1,21 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+typedef long long ll; // typedef for long long -> ll
+#define F first; // can define a macro like this, v[i].first -> v[i].F
+
+#define REP(i,a,b) for (int i=a; i<=b; i++); // macro that simplifies loops?!???!
+// for (int i=1;i<=n;i++) -> REP(i,1,n)
+
 //g++ -std=c++11 -O2 -Wall review.cpp -o review
 //run with ./review
-
-int main() {
-    // input_output();
-
-}
-
-void number_review() {
-    // most cp programs will use long long
-    long long x = 123456789123456789LL;
-
-    // floating point
-    printf("%.9f\n", x) // prints to 9 decimals
-}
 
 void io_review() {
     // typical cout/cin
@@ -33,7 +26,6 @@ void io_review() {
 
     // getting a full line
     // note: getline() reads whole line. cin takes chars up to first whitespace.
-    string s;
     getline(cin, s);
 
     // loop that keeps taking input!
@@ -46,4 +38,58 @@ void io_review() {
     // file io
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
+}
+
+void number_review() {
+    // most cp programs will use long long
+    ll x = 123456789123456789LL;
+    // printf("%lld", x);
+
+    // floating point
+    float y = 0.1234567890;
+    printf("%.9f\n", y); // prints to 9 decimals
+
+    // beware floating point errors!
+    // compare floating point nums and assume equal if difference is less than e
+    float a = 1.09999998, b=1.099999999;
+    if (abs(a-b) < 1e-9) {
+        cout << "yay, equal\n";
+    } else {
+        cout << "not equal? huh\n";
+    }
+}
+
+void math_review() {
+    // sum formula
+        // \sum^n_{x=1}x = \frac{n(n+1)}{2}
+        // \sum^n_{x=1}x^2 = \frac{n(n+1)(2n+1)}{6}
+    
+    // sum of arithmetic progression
+        // \frac{n(a+b)}{2}
+            // n = # of numbers, a = first number, b = last number
+    
+    // geometric progression
+        // \frac{bk-a}{k-1}
+            // a = first num, b = last num, k = ratio between consecutive nums
+
+    // harmonic sum
+        // \sum^n_{x=1} 1/x
+
+    // set theory
+        // intersection = elements in both A and B
+        // union = elements that are in A or B or both
+        // complement = elements of A not in B in the universal set
+        // difference = elements in A but not in B
+    
+    // logic
+        // negation
+        // conjunction
+        // disjunction
+        // implication
+        // equivalence
+}
+
+int main() {
+    // io_review();
+    // number_review();
 }
